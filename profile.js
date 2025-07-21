8e0199efdc3c0001e6dcb0432ef66415a893d2ad
Raheel Ahmed <raheel25031@gmail.com>
2025-07-21 19:10:32 +0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -14,46 +14,74 @@ const fetchUserData = async () =>{
     const email = document.getElementById("email")
     const DOB = document.getElementById("DOB")
     const phoneNo = document.getElementById("phoneNo")
+    const newImg = document.getElementById("newImg")
 
     firstName.value = userData.data().firstName;
     lastName.value = userData.data().lastName;
     email.value = userData.data().email;
     phoneNo.value = userData.data().phoneNo;
     DOB.value = userData.data().DOB;
+    newImg.src = userData.data().imgUrl;
     
 
 }
 
-const updataProfileFirstName = async () =>{
-    console.log("updateProfileData");
-    const editedName = prompt("Enter Updated first name")
-    
+    const updateUserData = async () => {
+        // console.log("updateUserData")
+        try {
+            const efirstName = document.getElementById("firstName")
+            const elastName = document.getElementById("lastName")
+            const ephoneNo = document.getElementById("phoneNo")
+
     const userUid = localStorage.getItem("user")
-    const updatedObj = {
-        firstName : editedName,
+
+
+
+        const updatedObj = {
+            firstName : efirstName.value,
+            lastName : elastName.value,
+            phoneNo : ephoneNo.value
+        }
+
+    await updateDoc(doc(db, "users", userUid), updatedObj)
+    alert("data add successfully")
+        } catch (error) {
+            console.log("Error updating user", error)
+        }
+
     }
 
-    await updateDoc(doc(db, "users", userUid), updatedObj);
-    fetchUserData();
 
-    
+    const fileHandlerPic = async () => {
+        try {
+            console.log("hello file")
+        console.log("profileImg",profileImg.files[0])
+        const file = profileImg.files[0]
 
-}
+        const formData = new FormData()
+        formData.append("file", file)
+        formData.append("upload_preset", "paraticeImg")
 
-const updateProfileLastName = async() => {
-    console.log("updateProfileLastName",updateProfileLastName)
+        const response = await fetch(`https://api.cloudinary.com/v1_1/dczwggcc9/upload`,{
+            method: "POST",
+            body: formData,
+        }).then((response) => response.json())
 
-    const editedLastName = prompt("ENter your last Name")
-    const userUid  = localStorage.getItem("user")
+        console.log("path", response.secure_url)
 
-    const updatedObj = {
-        lastName : editedLastName,
-    } 
+        const userUid = localStorage.getItem("user")
+        await updateDoc(doc(db, "users", userUid),{
+            imgUrl : response.secure_url
+        })
+        fetchUserData();
+        } catch (error) {
+            console.log("error", error)
+        }
+        
+    }
 
-    await updateDoc(doc(db, "users", userUid), updatedObj);
-    fetchUserData();
-}
 
-window.updataProfileFirstName = updataProfileFirstName
+    window.fileHandlerPic = fileHandlerPic
+
+    window.updateUserData = updateUserData
 window.fetchUserData = fetchUserData
-window.updateProfileLastName = updateProfileLastName
